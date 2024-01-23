398f2841c306932dc3e836ee123c9714f7e0301d
Lucas Nogueira <lucas@crabnebula.dev>
2024-01-23 13:25:16 -0300

    move packages

--- packages/tauri-plugin-drag-api/rollup.config.js
+++ packages/tauri-plugin-drag-api/rollup.config.js
@@ -50,7 +50,7 @@ export default [
       banner: "if ('__TAURI__' in window) {",
       // the last `}` closes the if in the banner
       footer: `Object.defineProperty(window.__TAURI__, '${pluginJsName}', { value: ${iifeVarName} }) }`,
-      file: "../../crates/tauri-plugin/src/api-iife.js",
+      file: "../../crates/tauri-plugin-drag/src/api-iife.js",
     },
     // and var is not guaranteed to assign to the global `window` object so we make sure to assign it
     plugins: [typescript(), terser(), nodeResolve()],
