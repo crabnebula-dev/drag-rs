5618174c3a6678e13ff5c7bec1dd8dfa723e4235
ayang <473033518@qq.com>
2024-10-15 13:15:32 +0800

    chore: some problems are optimized

--- examples/tauri-dragout/src/main.rs
+++ examples/tauri-dragout/src/main.rs
@@ -2,5 +2,5 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    tauri_app_dragout_lib::run()
+    tauri_plugin_drag_as_window_lib::run()
 }
--- examples/tauri/src/main.rs
+++ examples/tauri/src/main.rs
@@ -2,5 +2,5 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    tauri_app_lib::run()
+    tauri_plugin_drag_lib::run()
 }
